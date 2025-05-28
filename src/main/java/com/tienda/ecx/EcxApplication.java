04da7def0bbf5eb6732037d13ac73b8ea0a2a7f4
XIMERICK <xorozco111184@umanizales.edu.co>
2025-05-27 20:38:16 -0500

    XIMENA

---
+++ src/main/java/com/tienda/ecx/EcxApplication.java
@@ -0,0 +1,13 @@
+package com.tienda.ecx;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcxApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EcxApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/tienda/ecx/EcxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tienda.ecx;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcxApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
